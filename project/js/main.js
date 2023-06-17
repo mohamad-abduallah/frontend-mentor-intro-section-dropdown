62fb12b1b7d39ec2993074637774c3570ac40282
mohamad-abduallah <mohamadn@motory.com>
2023-06-17 20:35:04 +0300

    finish desktop desgin

--- project/js/main.js
+++ project/js/main.js
@@ -1,12 +1,16 @@
-const btn1 = document.querySelector('.nav-rotate-1');
-const btn2 = document.querySelector('.nav-rotate-2');
-const rotate1 = document.querySelector('.move1');
-const rotate2 = document.querySelector('.move2');
+const btn1 = document.querySelector('.nav-rotate-one');
+const btn2 = document.querySelector('.nav-rotate-tow');
+const rotate1 = document.querySelector('.rotate-one');
+const rotate2 = document.querySelector('.rotate-tow');
 
 btn1.addEventListener('click', function () {
     rotate1.classList.toggle('rotate');
+    rotate1.classList.add('rotate-one');
+    rotate2.classList.remove('rotate');
 });
 
 btn2.addEventListener('click', function () {
     rotate2.classList.toggle('rotate');
+    rotate2.classList.add('rotate-one');
+    rotate1.classList.remove('rotate');
 });
