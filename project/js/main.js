0177e8fb0a9483348eeb27d2304c9173ca0cea9c
mohamad-abduallah <mohamadn@motory.com>
2023-06-17 03:43:08 +0300

    stop in menu bar in dropdwon list

--- project/js/main.js
+++ project/js/main.js
@@ -1,6 +1,12 @@
-const show = document.querySelector('.nav-link');
-const slowlyShow = document.querySelector('.fa-solid');
+const btn1 = document.querySelector('.nav-rotate-1');
+const btn2 = document.querySelector('.nav-rotate-2');
+const rotate1 = document.querySelector('.move1');
+const rotate2 = document.querySelector('.move2');
 
-show.addEventListener('click', function () {
-    slowlyShow.classList.toggle('rotate');
+btn1.addEventListener('click', function () {
+    rotate1.classList.toggle('rotate');
+});
+
+btn2.addEventListener('click', function () {
+    rotate2.classList.toggle('rotate');
 });
