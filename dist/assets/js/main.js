0a078e8d30fb5c4106128038618e3686a02962a3
mohamad-abduallah <mohamadn@motory.com>
2023-06-19 13:31:58 +0300

    finish the jquery code

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,76 +1,32 @@
-const showListDescOne = document.querySelector('.nav-rotate-one');
-const showListDescTow = document.querySelector('.nav-rotate-tow');
-const rotateArrowOne = document.querySelector('.rotate-one');
-const rotateArrowTow = document.querySelector('.rotate-tow');
-const showFirstList = document.querySelector('.dropdown-list');
-const showSecondList = document.querySelector('.dropdown-list-tow');
+$(document).ready(function () {
+    //Dropdown list menu in desktop
+    $(".nav-rotate-one").click(function () {
+        $(".dropdown-list").toggleClass("show");
+        $(".nav-rotate-one").toggleClass("show");
+        $(".rotate-one").toggleClass("rotate");
 
-const btnMobileOne = document.querySelector('.btn-mobile-one');
-const btnMobileTow = document.querySelector('.btn-mobile-tow');
-const rotateMobile1 = document.querySelector('.rotate-mobile-one');
-const rotateMobile2 = document.querySelector('.rotate-mobile-tow');
-const showFirstListMobile = document.querySelector('.dropdown-mobile-one');
-const showSecondListMobile = document.querySelector('.dropdown-mobile-tow');
+        $(".dropdown-list-tow").removeClass("show");
+        $(".nav-rotate-tow").removeClass("show");
+        $(".rotate-tow").removeClass("rotate");
+    });
 
-const btnShowMenu = document.querySelector('.navbar-toggler');
-const addMenu = document.querySelector('.offcanvas-end');
-const showListMenu= document.querySelector('.navbar-collapse');
-const modalBackdrop = document.querySelector('.modal-backdrop');
+    $(".nav-rotate-tow").click(function () {
+        $(".dropdown-list-tow").toggleClass("show");
+        $(".nav-rotate-tow").toggleClass("show");
+        $(".rotate-tow").toggleClass("rotate");
 
+        $(".dropdown-list").removeClass("show");
+        $(".nav-rotate-one").removeClass("show");
+        $(".rotate-one").removeClass("rotate");
+    });
 
-//Show list in menu Desktop
-showListDescOne.addEventListener('click', function () {
-    showFirstList.classList.toggle('show');
-    showListDescOne.classList.toggle('show');
-    rotateArrowOne.classList.toggle('rotate');
+    //Show Menu Bar
+    $(".navbar-toggler").click(function () {
+        $('.offcanvas-end').toggleClass("show-menu");
+        $(".modal-backdrop").toggleClass("show disFlex");
+        $(".navbar-toggler").toggleClass("rotate-btn");
 
-    showSecondList.classList.remove('show');
-    showListDescTow.classList.remove('show');
-    rotateArrowTow.classList.remove('rotate');
+        var visibility = $('.offcanvas-end').css('visibility');
+        $('.offcanvas-end').css('visibility', visibility === 'visible' ? 'hidden' : 'visible');
+    });
 });
-showListDescTow.addEventListener('click', function () {
-    showSecondList.classList.toggle('show');
-    showListDescTow.classList.toggle('show');
-    rotateArrowTow.classList.toggle('rotate');
-
-    showFirstList.classList.remove('show');
-    showListDescOne.classList.remove('show');
-    rotateArrowOne.classList.remove('rotate');
-});
-
-// Show list menu in mobile
-btnMobileOne.addEventListener('click', function () {
-    showFirstListMobile.classList.toggle('show');
-    btnMobileOne.classList.toggle('show');
-    rotateMobile1.classList.toggle('rotate');
-
-    showSecondListMobile.classList.remove('show');
-    btnMobileTow.classList.remove('show');
-    rotateMobile2.classList.remove('rotate');
-});
-
-btnMobileTow.addEventListener('click', function () {
-    showSecondListMobile.classList.toggle('show');
-    btnMobileTow.classList.toggle('show');
-    rotateMobile2.classList.toggle('rotate');
-
-    showFirstListMobile.classList.remove('show');
-    btnMobileOne.classList.remove('show');
-    rotateMobile1.classList.remove('rotate');
-});
-
-
-//open the menu bar
-btnShowMenu.addEventListener('click', function () {
-    addMenu.classList.toggle('show-menu');
-    showListMenu.classList.toggle('show');
-    modalBackdrop.classList.toggle('show');
-    modalBackdrop.classList.toggle('disFlex');
-    btnShowMenu.classList.toggle('rotate-btn');
-    if (addMenu.style.visibility === 'visible') {
-        addMenu.style.visibility = 'hidden';
-    } else {
-        addMenu.style.visibility = 'visible';
-    }
-});
-
